2d631ecba9973562e7225c23d2803eb74751cdc0
Simona Coroama <simona.coroama@centric.eu>
2023-02-27 20:51:32 +0200

    Search box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import styles from "./App.css";
 import alanBtn from "@alan-ai/alan-sdk-web"
 
 const App=() => {
   const [products, setProducts] = useState([])
+  const alanBtnRef = useRef({}).current;
 
   useEffect((e) => {
-    var alanBtnInstance = alanBtn({
+    alanBtnRef.alanBtnInstance = alanBtn({
       showOverlayOnMicPermissionPrompt: true,
         key: '9abb912cf31f2d339140636b3b3b922f2e956eca572e1d8b807a3e2338fdd0dc/stage',
         onCommand: (commandData) => {
@@ -14,20 +15,40 @@ const App=() => {
             setProducts(commandData.data)
             console.log(commandData)
           } 
+          else if (commandData.command === "skirt") {
+            setProducts(commandData.data)
+            console.log(commandData)
+          } else if (commandData.command === "jacket") {
+            setProducts(commandData.data)
+            console.log(commandData)
+          } 
         },
         onButtonState: async function(status) {
           if (status === 'ONLINE') {
             if (!this.greetingWasSaid) {
-              await alanBtnInstance.activate();
-              alanBtnInstance.playText("Hello! I'm Alan. How can I help you?");
+              await alanBtnRef.alanBtnInstance.activate();
+              alanBtnRef.alanBtnInstance.playText("Hello! I'm Alan. How can I help you?");
+              
               this.greetingWasSaid = true
             }
           }
         }
     })
-    alanBtnInstance.setVisualState({screen:"main"})
+    alanBtnRef.alanBtnInstance.setVisualState({screen:"main"})
+   
   }, [])
 
+  function click (e){
+    if(e.keyCode === 13){
+      console.log('value', e.target.value);
+      if( e.target.value ===''){
+        setProducts([]);
+      }
+       alanBtnRef.alanBtnInstance.activate();
+       alanBtnRef.alanBtnInstance.sendText(e.target.value);
+   }
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -35,11 +56,11 @@ const App=() => {
       </header>
     
 
-    {/* {products.map((product) => (
+    {products.map((product) => (
       <div key={product.name}>
         {product.name}
       </div>
-    ))} */}
+    ))}
 
 <section id="deals">
 {products.map((product) => (
@@ -80,6 +101,9 @@ const App=() => {
     <button>BUY NOW</button>
   </section>
 </section>
+
+<div className="alanBtn"></div>
+<input type="text" onKeyDown={click}/>
     </div>
   )
 }
